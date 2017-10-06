b29af283d9813d9d8f28c3eed618fa8797b19d07
Michael Burns <michael.burns@civica.co.uk>
2017-10-06 15:46:16 +0100

    Adding a many to many relationship between Gig and User. Adding model and migration. Adding a web api controller and method to handle a users attendance at a Gig.

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,40 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Web.Http;
+using System.Linq;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int gigId)
+        {
+            var userId = User.Identity.GetUserId();
+            
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId))
+            {
+                return BadRequest("The attendance allready exists.");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = userId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
