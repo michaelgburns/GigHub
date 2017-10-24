006d2a06fc5a51cd3b9343007ee963cee9d98a8c
Michael Burns <michael.burns@civica.co.uk>
2017-10-24 16:56:17 +0100

    Refactoring code out of the Gigs api controller to the domain models. That is adding behaviour to the domain/data models.

--- GigHub/Controllers/Api/GigsController.cs
+++ GigHub/Controllers/Api/GigsController.cs
@@ -1,8 +1,9 @@
 ﻿using GigHub.Models;
 using Microsoft.AspNet.Identity;
-using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
+
 namespace GigHub.Controllers.Api
 {
     [Authorize]
@@ -19,34 +20,14 @@ namespace GigHub.Controllers.Api
         public IHttpActionResult Cancel(int id)
         {
             var userId     = User.Identity.GetUserId();
-            var gig        = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+            var gig        = _context.Gigs
+                            .Include(g => g.Attendances.Select(a => a.Attendee))
+                            .Single(g => g.Id == id && g.ArtistId == userId);
             
             if (gig.IsCanceled)
                 return NotFound();
-           
-            gig.IsCanceled = true;
 
-            var notification = new Notification
-            {
-                DateTime = DateTime.Now,
-                Gig      = gig,
-                Type     = NotificationType.GigCanceled
-            };
-
-            var attendees = _context.Attendances
-                .Where(a => a.GigId == gig.Id)
-                .Select(a => a.Attendee)
-                .ToList();
-
-            foreach (var attendee in attendees)
-            {
-                var userNotification = new UserNotification
-                {
-                    User = attendee,
-                    Notification = notification
-                };
-                _context.UserNotifications.Add(userNotification);
-            }
+            gig.Cancel();                      
 
             _context.SaveChanges();
 
--- GigHub/Models/UserNotification.cs
+++ GigHub/Models/UserNotification.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace GigHub.Models
@@ -7,16 +8,29 @@ namespace GigHub.Models
     {
         [Key]
         [Column(Order = 1)]
-        public string UserId { get; set; }
+        public string UserId { get; private set; }
 
         [Key]
         [Column(Order = 2)]
-        public int NotificationId { get; set; }
+        public int NotificationId { get; private set; }
 
         public bool IsRead { get; set; }
 
         // navigation properties to navigate to related properties.
-        public ApplicationUser User { get; set; }
-        public Notification Notification { get; set; }
+        public ApplicationUser User { get; private set; }
+        public Notification Notification { get; private set; }
+
+        protected UserNotification()
+        {
+        }
+        
+        public UserNotification(ApplicationUser user, Notification notification)
+        {
+            if (user == null) throw new ArgumentNullException("User");
+            if (notification == null) throw new ArgumentNullException("Notification");
+
+            User         = user;
+            Notification = notification;           
+        }
     }
 }
